3fd97143cec68e25daa1e765245727f4c8af2864
lavix5 <119361462+lavix5@users.noreply.github.com>
2024-11-01 09:42:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Freestyle football to footbag dictionary 
+
+This is a POC of a dictionary of trick names between football and footbag. It's simple shell script. It uses simply 2 files with tricks names. Don't edit those files as line numbers corresponds to the same trick in both sports. In the future I might, or I might not, add more tricks to the file, but for now it is just a POC.
+
+### Usage :
+
+Clone the repo:: 
+```sh
+$ git clone https://github.com/lavix5/football-footbag-dictionary.git && cd football-footbag-dictionary
+```
+Make script executable:
+
+```sh
+$ sudo chmod +x ./dictionary.sh
+```
+Run script with trick name:
+```sh
+$ ./dictionary.sh trick_name
+```
---
+++ dictionary.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+E_NOARGS=85
+E_BADARGS=86
+trick="$1"
+
+if [ -z "$1" ]
+then
+        echo "Usage: $0 trick_name"
+        exit $E_NOARGS
+fi
+
+if [ $# -gt 1 ]
+then
+        echo "Usage: $0 trick_name"
+        exit $E_BADARGS
+fi
+
+line_number_footbag=$(grep -n "$trick" footbag | cut -d : -f 1)
+line_number_football=$(grep -n "$trick" football | cut -d : -f 1)
+
+if [[ "$line_number_footbag" == "" && "$line_number_football" == "" ]]
+then
+	echo "Trick name $trick not found in dictionary"
+	exit 0
+fi
+
+if [[ "$line_number_footbag" != "" ]]
+then
+	echo "Footbag trick $trick is football trick $( head -n "$line_number_footbag" football | tail -1 ) "
+fi
+
+if [[ "$line_number_football" != "" ]]
+then
+	echo "Football trick $trick is footbag trick $( head -n "$line_number_football" footbag | tail -1 ) "
+fi
+
